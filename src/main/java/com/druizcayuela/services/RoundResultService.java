358914e5101e49c77c92cd8e6f75b8e105644e67
Daniel Ruiz Cayuela <druiz@financialforce.com>
2020-11-14 19:48:13 +0100

    ADD round result memory service implementation

---
+++ src/main/java/com/druizcayuela/services/RoundResultService.java
@@ -0,0 +1,17 @@
+package com.druizcayuela.services;
+
+import com.druizcayuela.domain.Move;
+import com.druizcayuela.domain.RoundResult;
+
+public interface RoundResultService extends CrudService<RoundResult>{
+
+    /**
+     * Although it's specified that one player always will be ROCK, I cannot hardcode my
+     * algorithm of checking who is the win
+     *
+     * @param onePlayerMove the one player move
+     * @param secondPlayerMove the second player move
+     * @return the round result
+     */
+    RoundResult evaluateMoves(final Move onePlayerMove, final Move secondPlayerMove);
+}
---
+++ src/main/java/com/druizcayuela/services/memory/RoundResultMemoryService.java
@@ -0,0 +1,38 @@
+package com.druizcayuela.services.memory;
+
+import com.druizcayuela.domain.Move;
+import com.druizcayuela.domain.Result;
+import com.druizcayuela.domain.RoundResult;
+import com.druizcayuela.services.RoundResultService;
+import org.springframework.stereotype.Service;
+
+/**
+ * The round memory service bean
+ *
+ * @author druizcayuela
+ *
+ */
+
+@Service
+public class RoundResultMemoryService extends AbstractMemoryService<RoundResult> implements RoundResultService {
+
+    @Override
+    public RoundResult evaluateMoves(Move onePlayerMove, Move secondPlayerMove) {
+
+        Result result;
+
+        if (onePlayerMove.beats(secondPlayerMove)) {
+            result = Result.ONE_WINS;
+        } else if (secondPlayerMove.beats(onePlayerMove)) {
+            result = Result.TWO_WINS;
+        } else {
+            result = Result.DRAW;
+        }
+
+        return RoundResult.builder()
+                .firstPlayerChoice(onePlayerMove.getOutput())
+                .secondPlayerChoice(secondPlayerMove.getOutput())
+                .result(result.getOutput())
+                .build();
+    }
+}
