9504c2fb61a3499d316d34a5cb3ae3c0317354b3
Daniel Ruiz Cayuela <druiz@financialforce.com>
2020-11-14 21:28:15 +0100

    UPDATE total result with new tests for the new upcoming service method

--- src/test/java/com/druizcayuela/services/memory/TotalResultMemoryServiceTest.java
+++ src/test/java/com/druizcayuela/services/memory/TotalResultMemoryServiceTest.java
@@ -1,5 +1,7 @@
 package com.druizcayuela.services.memory;
 
+import com.druizcayuela.domain.Result;
+import com.druizcayuela.domain.RoundResult;
 import com.druizcayuela.domain.TotalResult;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,16 +13,21 @@ public class TotalResultMemoryServiceTest {
 
     private TotalResultMemoryService totalResultMemoryService;
 
+    private final int INITIAL_ROUNDS_PLAYED = 3;
+    private final int INITIAL_WINS_FIRST_PLAYER = 1;
+    private final int INITIAL_WINS_SECOND_PLAYER = 1;
+    private final int INITIAL_TOTAL_DRAWS = 1;
+
 
     @Before
     public void setUp() {
 
         totalResultMemoryService = new TotalResultMemoryService();
         totalResultMemoryService.save(TotalResult.builder()
-                .roundsPlayed(3)
-                .winsFirstPlayer(1)
-                .winsSecondPlayer(1)
-                .totalDraws(1)
+                .roundsPlayed(INITIAL_ROUNDS_PLAYED)
+                .winsFirstPlayer(INITIAL_WINS_FIRST_PLAYER)
+                .winsSecondPlayer(INITIAL_WINS_SECOND_PLAYER)
+                .totalDraws(INITIAL_TOTAL_DRAWS)
                 .build());
     }
 
@@ -32,10 +39,10 @@ public class TotalResultMemoryServiceTest {
 
         // Then
         assertNotNull(totalResult);
-        assertEquals(Integer.valueOf(3), totalResult.getRoundsPlayed());
-        assertEquals(Integer.valueOf(1), totalResult.getWinsFirstPlayer());
-        assertEquals(Integer.valueOf(1), totalResult.getWinsSecondPlayer());
-        assertEquals(Integer.valueOf(1), totalResult.getTotalDraws());
+        assertEquals(Integer.valueOf(INITIAL_ROUNDS_PLAYED), totalResult.getRoundsPlayed());
+        assertEquals(Integer.valueOf(INITIAL_WINS_FIRST_PLAYER), totalResult.getWinsFirstPlayer());
+        assertEquals(Integer.valueOf(INITIAL_WINS_SECOND_PLAYER), totalResult.getWinsSecondPlayer());
+        assertEquals(Integer.valueOf(INITIAL_TOTAL_DRAWS), totalResult.getTotalDraws());
     }
 
     @Test
@@ -55,5 +62,62 @@ public class TotalResultMemoryServiceTest {
         assertEquals(Integer.valueOf(4), totalResult.getWinsFirstPlayer());
         assertEquals(Integer.valueOf(4), totalResult.getWinsSecondPlayer());
         assertEquals(Integer.valueOf(2), totalResult.getTotalDraws());
+    }
+
+    @Test
+    public void update_winsPlayer1() {
+
+        // Given
+        RoundResult roundResult = RoundResult.builder()
+                .result(Result.ONE_WINS.getOutput())
+                .build();
+
+        // When
+        TotalResult totalResult = totalResultMemoryService.update(roundResult);
+
+        // Then
+        assertNotNull(totalResult);
+        assertEquals(Integer.valueOf(INITIAL_ROUNDS_PLAYED + 1), totalResult.getRoundsPlayed());
+        assertEquals(Integer.valueOf(INITIAL_WINS_FIRST_PLAYER + 1), totalResult.getWinsFirstPlayer());
+        assertEquals(Integer.valueOf(INITIAL_WINS_SECOND_PLAYER), totalResult.getWinsSecondPlayer());
+        assertEquals(Integer.valueOf(INITIAL_TOTAL_DRAWS), totalResult.getTotalDraws());
+    }
+
+    @Test
+    public void update_winsPlayer2() {
+
+        // Given
+        RoundResult roundResult = RoundResult.builder()
+                .result(Result.TWO_WINS.getOutput())
+                .build();
+
+        // When
+        TotalResult totalResult = totalResultMemoryService.update(roundResult);
+
+        // Then
+        assertNotNull(totalResult);
+        assertEquals(Integer.valueOf(INITIAL_ROUNDS_PLAYED + 1), totalResult.getRoundsPlayed());
+        assertEquals(Integer.valueOf(INITIAL_WINS_FIRST_PLAYER), totalResult.getWinsFirstPlayer());
+        assertEquals(Integer.valueOf(INITIAL_WINS_SECOND_PLAYER + 1), totalResult.getWinsSecondPlayer());
+        assertEquals(Integer.valueOf(INITIAL_TOTAL_DRAWS), totalResult.getTotalDraws());
+    }
+
+    @Test
+    public void update_draw() {
+
+        // Given
+        RoundResult roundResult = RoundResult.builder()
+                .result(Result.DRAW.getOutput())
+                .build();
+
+        // When
+        TotalResult totalResult = totalResultMemoryService.update(roundResult);
+
+        // Then
+        assertNotNull(totalResult);
+        assertEquals(Integer.valueOf(INITIAL_ROUNDS_PLAYED + 1), totalResult.getRoundsPlayed());
+        assertEquals(Integer.valueOf(INITIAL_WINS_FIRST_PLAYER), totalResult.getWinsFirstPlayer());
+        assertEquals(Integer.valueOf(INITIAL_WINS_SECOND_PLAYER), totalResult.getWinsSecondPlayer());
+        assertEquals(Integer.valueOf(INITIAL_TOTAL_DRAWS + 1), totalResult.getTotalDraws());
     }
 }
