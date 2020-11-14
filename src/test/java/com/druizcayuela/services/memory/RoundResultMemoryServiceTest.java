4529cbeffe53d3e89fc2fa50b190ba6ed349b3e0
Daniel Ruiz Cayuela <druiz@financialforce.com>
2020-11-14 19:41:21 +0100

    ADD unit tests of RoundResultMemory Service Test

---
+++ src/test/java/com/druizcayuela/services/memory/RoundResultMemoryServiceTest.java
@@ -0,0 +1,68 @@
+package com.druizcayuela.services.memory;
+
+import com.druizcayuela.domain.Move;
+import com.druizcayuela.domain.Result;
+import com.druizcayuela.domain.RoundResult;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RoundResultMemoryServiceTest {
+
+    private RoundResultMemoryService roundResultMemoryService;
+
+
+    @Before
+    public void setUp() throws Exception {
+        roundResultMemoryService = new RoundResultMemoryService();
+    }
+
+    @Test
+    public void evaluateMoves_winPlayerOne() {
+
+        // Given
+        Move playerOne = Move.PAPER;
+        Move playerTwo = Move.ROCK;
+
+        // When
+        RoundResult roundResult = roundResultMemoryService.evaluateMoves(playerOne, playerTwo);
+
+        // Then
+        assertEquals(Result.ONE_WINS.getOutput(), roundResult.getResult());
+        assertEquals(playerOne.getOutput(), roundResult.getFirstPlayerChoice());
+        assertEquals(playerTwo.getOutput(), roundResult.getSecondPlayerChoice());
+    }
+
+    @Test
+    public void evaluateMoves_winPlayerTwo() {
+
+        // Given
+        Move playerOne = Move.SCISSORS;
+        Move playerTwo = Move.ROCK;
+
+        // When
+        RoundResult roundResult = roundResultMemoryService.evaluateMoves(playerOne, playerTwo);
+
+        // Then
+        assertEquals(Result.TWO_WINS.getOutput(), roundResult.getResult());
+        assertEquals(playerOne.getOutput(), roundResult.getFirstPlayerChoice());
+        assertEquals(playerTwo.getOutput(), roundResult.getSecondPlayerChoice());
+    }
+
+    @Test
+    public void evaluateMoves_draw() {
+
+        // Given
+        Move playerOne = Move.ROCK;
+        Move playerTwo = Move.ROCK;
+
+        // When
+        RoundResult roundResult = roundResultMemoryService.evaluateMoves(playerOne, playerTwo);
+
+        // Then
+        assertEquals(Result.DRAW.getOutput(), roundResult.getResult());
+        assertEquals(playerOne.getOutput(), roundResult.getFirstPlayerChoice());
+        assertEquals(playerTwo.getOutput(), roundResult.getSecondPlayerChoice());
+    }
+}
